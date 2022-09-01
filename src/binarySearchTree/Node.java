34950938d7497d06cdd9b09c372f7e0f72220a16
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-02 00:41:25 +0530

    [Veer] Added : Welcome Message

---
+++ src/binarySearchTree/Node.java
@@ -0,0 +1,22 @@
+package binarySearchTree;
+/**
+ * 
+ * @author Veer.Singa
+ *
+ */
+//Class Node 
+public class Node {
+	/*
+	 * With 3 variable data,left and right.
+	 * left and right are of Node type variables.
+	 * Created a Constructor passing integer data. 
+	 */
+	int data;
+	Node left;
+	Node right;
+	public Node(int data) {
+		this.data = data;
+	}
+	
+
+}
---
+++ src/binarySearchTree/TreeMain.java
@@ -0,0 +1,15 @@
+package binarySearchTree;
+
+public class TreeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		welcome();
+
+	}
+	static void welcome() {
+		System.out.println("Welcome to Binary Search Tree Program");
+		System.out.println("=====================================");
+	}
+
+}
