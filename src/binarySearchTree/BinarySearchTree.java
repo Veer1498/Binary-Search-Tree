d46bf8d4b5f92e33a136a0eb7cb4c248c6467e48
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-02 01:47:43 +0530

    [Veer] Added : Ability to Create Binary Tree and Add Elements

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -1,5 +1,65 @@
 package binarySearchTree;
-
+/**
+ * 
+ * @author Veer.Singa
+ *
+ */
 public class BinarySearchTree {
+	/*
+	 * Created root of type Node
+	 * Created Functions AddNode and PrintNode
+	 * 
+	 */
+	Node root;
+	
+	public void addNode(int data) {
+		/**
+		 * Node Object Created with Constructor Passing data
+		 * If Else Condition Needed.
+		 * If - root equal to null, root = node and returns from function
+		 * Else - parent and current stores root
+		 * 		  While Looping till current node equals null.
+		 * While Loop - If   - node is greater than present data it moves to right leaf node
+		 * 				Else - data moves to left leaf node.
+		 * 
+		 */
+		Node node = new Node(data);
+		node.data = data;
+		node.left = null;
+		node.right = null;
+		
+		if(root == null) {
+			root = node;
+			return;
+		}
+		else {
+			Node parent = root;
+			Node current = root;
+			while(current != null) {
+				if(node.data > current.data) {
+					current = current.right;
+				}
+				else {
+					current = current.left;
+				}
+			}
+			if(node.data > parent.data) {
+				parent.right = node;
+			}
+			else {
+				parent.left = node;
+			}
+		}
+	}
+	//To Print Tree its Function
+	public void printTree(Node node) {
+		if(node.left != null) {
+			printTree(node.left);
+		}
+		System.out.println(node.data+" ");
+		if(node.right != null) {
+			printTree(node.right);
+		}
+	}
 
 }
--- src/binarySearchTree/TreeMain.java
+++ src/binarySearchTree/TreeMain.java
@@ -1,12 +1,29 @@
 package binarySearchTree;
-
+/**
+ * 
+ * @author Veer.Singa
+ *
+ */
+//Main Class of Binary Tree
 public class TreeMain {
-
+	//Main Function
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/*
+	 * Added Welcome Message
+	 * Adding Nodes to Binary Tree by Calling its Object.Method
+	 * Printing The Binary Tree Using InOrder Technique.
+	 * 
+	 */
 		welcome();
-
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.addNode(56);
+		tree.addNode(30);
+		tree.addNode(70);
+		System.out.println("Binary Search Tree After Adding Elements ");
+		tree.printTree(tree.root);
 	}
+	
+	//Welcome Function
 	static void welcome() {
 		System.out.println("Welcome to Binary Search Tree Program");
 		System.out.println("=====================================");
